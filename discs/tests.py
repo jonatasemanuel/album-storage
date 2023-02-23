04e2625630fb783284de73f70f35800de985c123
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-02-22 22:58:42 -0300

    test: discs, artists branch:fbv

--- discs/tests.py
+++ discs/tests.py
@@ -20,7 +20,7 @@ class DiscURLsTest(TestCase):
 
     def test_artists_url_is_correct(self):
         url = reverse('artists')
-        self.assertEqual(url, '/artists/')
+        self.assertEqual(url, '/artists/albums/')
 
     def test_artist_detail_url_is_correct(self):
         url = reverse('artist-detail', kwargs={'slug': 'test'})
@@ -37,5 +37,84 @@ class DiscURLsTest(TestCase):
 
 class DiscViewsTest(TestCase):
 
-    def test_disc_home_view_function_is_correct(self):
-        ...
+    def test_discs_home_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_discs_home_loads_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'discs/home.html')
+
+    def test_discs_home_view_function_is_correct(self):
+        view = resolve(reverse('home'))
+        self.assertIs(view.func, views.home_view)
+
+    def test_discs_all_discs_status_code_200_ok(self):
+        response = self.client.get(reverse('discs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_discs_all_discs_loads_correct_template(self):
+        response = self.client.get(reverse('discs'))
+        self.assertTemplateUsed(response, 'discs/discs.html')
+
+    def test_discs_all_discs_function_is_correct(self):
+        view = resolve(reverse('discs'))
+        self.assertIs(view.func, views.discs_all_discs)
+
+    def test_discs_all_template_shows_no_artists_found_if_no_artists(self):
+        response = self.client.get(reverse('discs'))
+        self.assertIn(
+            'Not Found',
+            response.content.decode('utf-8')
+        )
+
+    def test_discs_detail_view_function_is_correct(self):
+        view = resolve(reverse('disc-detail', kwargs={'slug': 'batata'}))
+        self.assertIs(view.func, views.discs_detail_disc)
+
+    def test_discs_detail_view_returns_404_if_no_discs_found(self):
+        response = self.client.get(
+            reverse('disc-detail', kwargs={'slug': 'batata'})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_discs_category_view_function_is_correct(self):
+        view = resolve(reverse('category',
+                       kwargs={'category_slug': 'hip-hop'}))
+        self.assertIs(view.func, views.category_albums)
+
+    def test_discs_category_view_return_404_if_no_discs_found(self):
+        response = self.client.get(
+            reverse('category', kwargs={'category_slug': 'batata'})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_discs_artist_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('artists'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_discs_artist_view_loads_correct_template(self):
+        response = self.client.get(reverse('artists'))
+        self.assertTemplateUsed(response, 'discs/artists.html')
+
+    def test_discs_artists_template_shows_no_artists_found_if_no_artists(self):
+        response = self.client.get(reverse('artists'))
+        self.assertIn(
+            'algo',
+            response.content.decode('utf-8')
+        )
+
+    def test_discs_artists_view_function_is_correct(self):
+        view = resolve(reverse('artists'))
+        self.assertIs(view.func, views.artist_list)
+
+    def test_discs_artists_detail_view_is_correct(self):
+        view = resolve(reverse('artist-detail',
+                               kwargs={'artist_slug': 'bk'}))
+        self.assertIs(view.func, views.artist_detail)
+
+    def test_discs_artist_view_returns_404_if_no_artists_found(self):
+        response = self.client.get(
+            reverse('artists', kwargs={'artist_slug': 'batata'})
+        )
+        self.assertEqual(response.status_code, 404)
